c1be254224f12f7c99a55103cc877184b5b5985d
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 16:27:58 +0530

    ExpenseCategory.java

--- src/entity/ExpenseCategory.java
+++ src/entity/ExpenseCategory.java
@@ -1,1 +1,13 @@
-...
+package entity;
+
+public class ExpenseCategory {
+    private int categoryId;
+    private String categoryName;
+
+    public ExpenseCategory() {}
+    public ExpenseCategory(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+}
