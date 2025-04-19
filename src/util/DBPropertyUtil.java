26989fc33a35e20b966f3a4097439beb7dd385ce
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 16:37:40 +0530

    DBPropertyUtil.java

--- src/util/DBPropertyUtil.java
+++ src/util/DBPropertyUtil.java
@@ -1,1 +1,19 @@
-...
+package util;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DBPropertyUtil {
+    public static String getPropertyString(String filename) {
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream(filename));
+            return "jdbc:mysql://" + props.getProperty("host") + ":" + props.getProperty("port") + "/" + props.getProperty("dbname") +
+                    "?user=" + props.getProperty("username") +
+                    "&password=" + props.getProperty("password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
