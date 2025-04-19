ce6df4fc000d5549768d1fe413752b0937dde8ea
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 16:37:16 +0530

    DBConnUtil.java

--- src/util/DBConnUtil.java
+++ src/util/DBConnUtil.java
@@ -1,1 +1,20 @@
-...
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnUtil {
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                String connStr = DBPropertyUtil.getPropertyString("db.properties");
+                connection = DriverManager.getConnection(connStr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
