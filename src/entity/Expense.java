09eab7ed6be0a4c0f5a55aab26992619709b7b95
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 16:27:08 +0530

    Expense.java

--- src/entity/Expense.java
+++ src/entity/Expense.java
@@ -1,1 +1,23 @@
-...
+package entity;
+
+import java.sql.Date;
+
+public class Expense {
+    private int expenseId;
+    private int userId;
+    private double amount;
+    private int categoryId;
+    private Date date;
+    private String description;
+
+    public Expense() {}
+    public Expense(int expenseId, int userId, double amount, int categoryId, Date date, String description) {
+        this.expenseId = expenseId;
+        this.userId = userId;
+        this.amount = amount;
+        this.categoryId = categoryId;
+        this.date = date;
+        this.description = description;
+    }
+
+}
