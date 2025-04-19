9b04f0f62e99b765569224bdecd38835d08a539f
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 16:30:13 +0530

    IFinanceRepository.java

--- src/dao/IFinanceRepository.java
+++ src/dao/IFinanceRepository.java
@@ -1,1 +1,14 @@
-...
+package dao;
+
+import entity.User;
+import entity.Expense;
+import java.util.List;
+
+public interface IFinanceRepository {
+    boolean createUser(User user);
+    boolean deleteUser(int userId);
+    boolean createExpense(Expense expense);
+    boolean deleteExpense(int expenseId);
+    boolean updateExpense(int userId, Expense expense);
+    List<Expense> getAllExpenses(int userId);
+}
