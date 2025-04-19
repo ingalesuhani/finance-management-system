b512822b7fa4d3c37b06ef795ca913b522d01f0d
ingalesuhani <166195642+ingalesuhani@users.noreply.github.com>
2025-04-19 16:26:24 +0530

    User.java

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,1 +1,18 @@
-...
+package entity;
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {}
+    public User(int userId, String username, String password, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+   
+}
